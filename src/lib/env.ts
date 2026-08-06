38d4452bc2d71ce50450afb54677db82ca56cfbe
UdumiziSolomon <solomonudumizi@gmail.com>
2026-08-06 06:05:00 +0100

    chore: env fix

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,8 +2,8 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const env = createEnv({
-  server: { API_SECRET: z.string().min(1) },
-  client: { NEXT_PUBLIC_API_URL: z.string().url() },
+  server: { API_SECRET: z.string().min(1).optional() },
+  client: { NEXT_PUBLIC_API_URL: z.string().url().optional() },
   runtimeEnv: {
     API_SECRET: process.env.API_SECRET,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
